1552725a3f6c21cfc867c94357693c2534662fa5
Ridwan Mulyadi <ridwan.mulyadi@live.com>
2025-05-15 11:04:14 +0700

    run_dev.sh

---
+++ run_dev.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -euo pipefail
+
+temp_dir=$(mktemp -d /tmp/test_proj_mycli.XXXXXX)
+
+script_dir=$(readlink -f $(dirname $0))
+
+cat <<EOF > "$temp_dir/mycli"
+#!/bin/bash
+
+cd "$script_dir"
+python -m mycli.main "\$@"
+EOF
+
+chmod +x "$temp_dir/mycli"
+
+export PATH="$temp_dir":$PATH
+
+db "$@"
+
+rm -rf "$temp_dir"
